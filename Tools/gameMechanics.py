c395f1b51cea6b36e1edd83a40720b6589c4ee0d
AkashWaran <akash.waran@gmail.com>
2014-12-26 23:27:54 -0500

    .

--- Tools/gameMechanics.py
+++ Tools/gameMechanics.py
@@ -11,24 +11,17 @@ class LayoutUtilities :
 	self.width = map_contents['width']
 	self.hills = map_contents['hills']
 	self.walls = map_contents['walls']
-	self.freeCells = Set()
+	self.freeCells = []
 	for i in range(self.height) :
 	    for j in range(self.width) :
 		if (i, j) not in self.hills and (i, j) not in self.walls :
-		    self.freeCells.add((i, j))
+		    self.freeCells.append((i, j))
 	self.ants = {}
 	for player in config.PLAYERS :
 	    self.ants.update({player:[]})
 
     def getRandomFreeCell(self) :
-	return random.sample(self.freeCells, 1)[0]
-
-def tuple_hook(obj) :
-    modified_obj = {}
-    for key in obj :
-	if key in ["hills", "walls"] :
-	    val = [tuple(l) for l in obj[key]]
-	    modified_obj[key] = val
-	else :
-	    modified_obj[key] = obj[key]
-    return modified_obj
+	try :
+	    return random.choice(self.freeCells)
+	except IndexError :
+	    return None
